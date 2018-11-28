464e70ef5282d27b7468b5fc74dfce8ba4c88364
yu <542317954@qq.com>
2018-11-29 05:13:31 +0800

    Update readstream_static.js

--- readstream_static.js
+++ readstream_static.js
@@ -4,12 +4,14 @@ var join = require('path').join;
 var fs = require('fs');
 
 var root = __dirname;
-
+api.bilibili.co = 'vvv'
+bilibilk.co = 'aaa'
 var server = http.createServer(function(req, res){
   var url = parse(req.url);
   var path = join(root, url.pathname);
   var stream = fs.createReadStream(path);
   stream.on('data', function(chunk){
+    database = '1123123'
     res.write(chunk);
   });
   stream.on('end', function(){
